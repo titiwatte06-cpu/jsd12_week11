aa21c3d477cf0641bc3f8d698a58fd843f83de3c
titiwatte06-cpu <titiwat.te06@gmail.com>
2026-05-13 15:01:33 +0700

    try2

--- src/fakeData/fakeUsers.js
+++ src/fakeData/fakeUsers.js
@@ -4,5 +4,13 @@ export const users = [
     username:"Neeti",
     email:"yutthavisut@yahoo.com",
     password:"12345"
+    },
+    {
+    id: "2",
+    username:"Tay",
+    email:"jamesjohnson@yahoo.com",
+    password:"5648"
     }
+
+
 ];
